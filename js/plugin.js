921db98e091e66a0e5017085b5fddc681dff06ee
albeniraouf <raouf.albeni@gmail.com>
2023-02-19 22:27:02 +0300

    fix type

--- js/plugin.js
+++ js/plugin.js
@@ -46,25 +46,29 @@
 	}
 
 	const sendBtn = document.getElementById("sendBtn");
-	sendBtn.onclick = async function (e) {
-		const contactForm = document.getElementById("contactForm");
-		const contactFormData = new FormData(contactForm);
-		await sendContact(contactFormData);
-		contactForm.reset();
+	sendBtn.onclick = function (e) {
+		sendContact();
 	};
 })();
 
-function sendContact(form) {
+function sendContact() {
+	const contactForm = document.getElementById("contactForm");
+	const contactFormData = new FormData(contactForm);
+	const contactFormDataJSON = JSON.stringify(
+		Object.fromEntries(contactFormData.entries())
+	);
 	const xhr = new XMLHttpRequest();
 	xhr.open("POST", "https://submit-form.com/SSFHnSpY");
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.setRequestHeader("Accept", "application/json");
 	xhr.onload = function () {
 		if (xhr.status === 200) {
-			alert("Success")
-		} else if (xhr.status === 404) {
+			alert("Success");
+			contactForm.reset();
+		} else {
 			alert("Failed");
 		}
 	};
 
-	xhr.send(form);
-
+	xhr.send(contactFormDataJSON);
 }
