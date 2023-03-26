6f1033ce7ac26528cadfff46347a07c228de9fc8
albeniraouf <raouf.albeni@yahoo.com>
2023-03-26 19:36:12 +0300

    Major Update

--- js/plugin.js
+++ js/plugin.js
@@ -1,57 +1,4 @@
-/*---------------------------------------------------------------------
-	File Name: plugin.js
----------------------------------------------------------------------*/
-
-/* Avoid `console` errors in browsers that lack a console.
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-(function () {
-	var method;
-	var noop = function () {};
-	var methods = [
-		"assert",
-		"clear",
-		"count",
-		"debug",
-		"dir",
-		"dirxml",
-		"error",
-		"exception",
-		"group",
-		"groupCollapsed",
-		"groupEnd",
-		"info",
-		"log",
-		"markTimeline",
-		"profile",
-		"profileEnd",
-		"table",
-		"time",
-		"timeEnd",
-		"timeline",
-		"timelineEnd",
-		"timeStamp",
-		"trace",
-		"warn",
-	];
-	var length = methods.length;
-	var console = (window.console = window.console || {});
-
-	while (length--) {
-		method = methods[length];
-
-		// Only stub undefined methods.
-		if (!console[method]) {
-			console[method] = noop;
-		}
-	}
-
-	const sendBtn = document.getElementById("sendBtn");
-	sendBtn.onclick = function (e) {
-		sendContact();
-	};
-})();
-
-function sendContact() {
+const sendContact = () => {
 	const contactForm = document.getElementById("contactForm");
 	const contactFormData = new FormData(contactForm);
 	const contactFormDataJSON = JSON.stringify(
@@ -61,7 +8,7 @@ function sendContact() {
 	xhr.open("POST", "https://submit-form.com/SSFHnSpY");
 	xhr.setRequestHeader("Content-Type", "application/json");
 	xhr.setRequestHeader("Accept", "application/json");
-	xhr.onload = function () {
+	xhr.onload = () => {
 		if (xhr.status === 200) {
 			alert("Success");
 			contactForm.reset();
@@ -72,3 +19,10 @@ function sendContact() {
 
 	xhr.send(contactFormDataJSON);
 }
+
+(() => {
+	const sendBtn = document.getElementById("sendBtn");
+	sendBtn.onclick = () => {
+		sendContact();
+	};
+})();
