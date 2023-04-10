397a8b28247c06ace227f6a4594835e2be823d4d
albeniraouf <raouf.albeni@yahoo.com>
2023-04-10 20:06:34 +0300

    updates

--- js/plugin.js
+++ js/plugin.js
@@ -18,11 +18,30 @@ const sendContact = () => {
 	};
 
 	xhr.send(contactFormDataJSON);
-}
+};
 
 (() => {
 	const sendBtn = document.getElementById("sendBtn");
 	sendBtn.onclick = () => {
 		sendContact();
 	};
+
+	const usesDarkMode =
+		window.matchMedia("(prefers-color-scheme: dark)").matches || false;
+	const favicon = document.getElementById("favicon");
+
+	function switchIcon(usesDarkMode) {
+		if (usesDarkMode) {
+			favicon.href = "images/programecho-dark.svg";
+		} else {
+			favicon.href = "images/programecho-light.svg";
+		}
+	}
+
+	window
+		.matchMedia("(prefers-color-scheme: dark)")
+		.addEventListener("change", (e) => switchIcon(e.matches));
+	switchIcon(usesDarkMode);
 })();
+
+
